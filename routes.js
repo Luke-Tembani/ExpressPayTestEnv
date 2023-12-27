51dd4119e149cbe19f3a1167b91d8680c50c3ec4
LukeFj <112558889+LukeFj@users.noreply.github.com>
2023-12-27 16:03:16 +0200

    RESPONSE CODES

--- routes.js
+++ routes.js
@@ -2,11 +2,14 @@ const router = require("express").Router();
 
 router.get("/",(req,res)=>{
     res.send("App is live");
+    res.status(200).send("Ok");
+
 });
 
 
 router.post("/transaction_results",(req,res)=>{
     console.log(req.body);
+    res.status(200).send("Ok");
 });
 
 
