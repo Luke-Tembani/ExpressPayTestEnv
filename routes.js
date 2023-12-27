b0be6841b28ce107f76caffe1dd2bc2037bf9df8
LukeFj <112558889+LukeFj@users.noreply.github.com>
2023-12-27 15:59:56 +0200

    default route

--- routes.js
+++ routes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
+router.get("/",(req,res)=>{
+    res.send("App is live");
+});
+
 
 router.post("/transaction_results",(req,res)=>{
     console.log(req.body);
